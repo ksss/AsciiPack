6b205c0ae341587a38b9b9ce9e4ba5bed070d5f6
yuki kurihara <yuki@yuki-no-MacBook-Air.local>
2013-10-05 14:08:44 +0900

    refactoring

--- ruby/lib/asciipack/typemap.rb
+++ ruby/lib/asciipack/typemap.rb
@@ -1,35 +1,41 @@
 module AsciiPack
   class TypeMap
+    @@typemap = {
+      :int4 =>    'a',
+      :int8 =>    'b',
+      :int16 =>   'c',
+      :int32 =>   'd',
+      :int64 =>   'e',
+      :uint4 =>   'f',
+      :uint8 =>   'g',
+      :uint16 =>  'h',
+      :uint32 =>  'i',
+      :uint64 =>  'j',
+      :float32 => 'k',
+      :float64 => 'l',
+      :str4 =>    'm',
+      :str8 =>    'n',
+      :str16 =>   'o',
+      :str32 =>   'p',
+      :map =>     'q',
+      :array =>   'r',
+      :nil =>     's',
+      :false =>   't',
+      :true =>    'u',
+    }
     class << self
-      {
-        :int4 =>    'a',
-        :int8 =>    'b',
-        :int16 =>   'c',
-        :int32 =>   'd',
-        :int64 =>   'e',
-        :uint4 =>   'f',
-        :uint8 =>   'g',
-        :uint16 =>  'h',
-        :uint32 =>  'i',
-        :uint64 =>  'j',
-        :float32 => 'k',
-        :float64 => 'l',
-        :str4 =>    'm',
-        :str8 =>    'n',
-        :str16 =>   'o',
-        :str32 =>   'p',
-        :map =>     'q',
-        :array =>   'r',
-        :nil =>     's',
-        :false =>   't',
-        :true =>    'u',
-      }.each { |name, char|
+      def typemap
+        @@typemap
+      end
+
+      @@typemap.each do |name, char|
         module_eval %{
           def #{name}
             "#{char}"
           end
         }
-      }
+      end
+
     end
   end
 end
