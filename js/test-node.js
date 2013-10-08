505d2da88034a8e6de0953800864e70c3223e424
ksss <co000ri@gmail.com>
2013-10-08 20:49:32 +0900

    brushup test

--- js/test-node.js
+++ js/test-node.js
@@ -3,7 +3,13 @@
 var global = (function(){ return this })();
 var AsciiPack = require('./asciipack').AsciiPack;
 var assert = require('./assert').assert;
+var util = require('util');
 
 global.AsciiPack = AsciiPack;
 global.assert = assert;
-require('./test').run();
+require('./test').run(function(){
+  util.print('.');
+}, function(){
+  util.print('\n');
+  console.log('^[[32mall test done.^[[m');
+});
