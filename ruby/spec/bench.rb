3049fd5cd956a7881f5c2940206212ec0fd493a5
ksss <co000ri@gmail.com>
2013-10-09 17:18:51 +0900

    to detail

--- ruby/spec/bench.rb
+++ ruby/spec/bench.rb
@@ -6,32 +6,49 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'asciipack'
 require 'json'
 require 'msgpack'
-require 'benchmark'
 
-value = {}
-30.times { |i|
-  value[i] = case i % 5
-             when 0; 0xffffffff
-             when 1; " " * 0xffff
-             when 2; [1.1, -1.1, 1/3, 5]
-             when 3; JSON.parse(value.to_json)
-             when 4; nil
-             end
-}
-json = JSON.generate value
-ap = AsciiPack.pack value
-ms = Marshal.dump value
-msg = MessagePack.pack value
-n = 10
+def json_asciipack(name, obj)
+  obj = [obj]
+  json = obj.to_json
+  ap = AsciiPack.pack obj
+  ms = Marshal.dump obj
+  msg = MessagePack.pack obj
 
-Benchmark.bm do |x|
-  x.report("JSON.generate") {n.times{ JSON.generate value }}
-  x.report("JSON.parse") {n.times{ JSON.parse json }}
-  x.report("AsciiPack.pack") {n.times{ AsciiPack.pack value }}
-  x.report("AsciiPack.unpack") {n.times{ AsciiPack.unpack ap }}
-  x.report("Marshal.dump") {n.times{ Marshal.dump value }}
-  x.report("Marshal.load") {n.times{ Marshal.load ms }}
-  x.report("MessagePack.pack") {n.times{ MessagePack.pack value }}
-  x.report("MessagePack.unpack") {n.times{ MessagePack.unpack msg }}
+  p '[' + name + ']'
+  bench "AsciiPack.pack" do AsciiPack.pack obj end
+  bench "AsciiPack.unpack" do AsciiPack.unpack ap end
+  bench "JSON.generate" do obj.to_json end
+  bench "JSON.parse" do JSON.parse json end
+  bench "Marshal.dump" do Marshal.dump obj end
+  bench "Marshal.load" do Marshal.load ms end
+  bench "MessagePack.pack" do MessagePack.pack obj end
+  bench "MessagePack.unpack" do MessagePack.unpack msg end
+  p({
+    :ap => ap.length,
+    :json => json.length,
+    :marshal => ms.length,
+    :msgpack => msg.length
+  })
 end
+
+def bench(name)
+  t = Time.now
+  1000.times {
+    yield
+  }
+  p name + ': ' + (Time.now - t).to_s + 'ms'
+end
+
+{
+  "positive fixint" => 0,
+  "uint 4" => 16,
+  "fixbin" => "",
+  "bin 8" => '0123456789abcdef',
+  "float 64" => 1/3,
+  "map 4" => {},
+  "array 4" => [],
+  "nil" => nil,
+}.each { |key, value|
+  json_asciipack key, value
+}
 
