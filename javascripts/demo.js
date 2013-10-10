ebe769a224ae504b77e614583bbe4d94736fc2ce
ksss <co000ri@gmail.com>
2013-10-10 10:55:08 +0900

    fix typo

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -1,14 +1,18 @@
 $(function(){
   var $demo_asciipack = $('#demo-asciipack');
   var $demo_json = $('#demo-json');
+  var json = $('#demo-json').val();
+  var obj = JSON.parse(json);
+  var ap = AsciiPack.pack(obj);
 
   $demo_json.focus();
+  $demo_asciipack.html(ap);
 
   $demo_json.on('keyup', function(){
-    var json = $('#demo-json').val();
+    json = $('#demo-json').val();
     try {
-      var obj = JSON.parse(json);
-      var ap = AsciiPack.pack(obj);
+      obj = JSON.parse(json);
+      ap = AsciiPack.pack(obj);
       $demo_asciipack.html(ap);
     } catch (ex) {
     }
